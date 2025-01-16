73fe661b28c3c43abb3f49573a78a72296190acf
Kathleen <67641959+cyberkathleen@users.noreply.github.com>
2025-01-16 14:56:16 +0100

    Add showPage function

--- js/script.js
+++ js/script.js
@@ -3,22 +3,45 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
+/***
+* `showPage` function
+* This function will create and insert/append the elements needed to display a "page" of nine students
+* @param {Array} list - The array of student objects
+* @param {Int} page - The requested page number
+***/
+function showPage(list, page) {
+  const studentsPerPage = 9;
+  
+  // Start and end index of list items to be displayed on the page
+  const start = (page * studentsPerPage) - studentsPerPage;
+  const end = page * studentsPerPage;
 
+  // Select display area and empty it
+  const ul = document.querySelector('.student-list');
+  ul.innerHTML = "";
 
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
+  // Create and insert student elements to be displayed
+  for (let i = 0; i < list.length; i++) {
+    if (i >= start && i < end) {
+      const html = `
+        <li class="student-item cf">
+          <div class="student-details">
+            <img class="avatar" src="${list[i].picture.thumbnail}" alt="Profile Picture of ${list[i].name.first} ${list[i].name.last}">
+            <h3>${list[i].name.first} ${list[i].name.last}</h3>
+            <span class="email">${list[i].email}</span>
+          </div>
+          <div class="joined-details">
+            <span class="date">Joined ${list[i].registered.date}</span>
+          </div>
+        </li>
+      `;
 
+      ul.insertAdjacentHTML('beforeend', html);
+    }
+  }
+}
 
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
-
-
+showPage(data, 1);
 
 /*
 Create the `addPagination` function
